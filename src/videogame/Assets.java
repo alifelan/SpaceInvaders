68c46edba62d08a3e69ffdb1f082b8fdc4a13b13
JCFFlores <josecruzflores.fl@gmail.com>
2018-03-04 21:33:57 -0600

    Add menu array

--- src/videogame/Assets.java
+++ src/videogame/Assets.java
@@ -13,6 +13,7 @@ import java.awt.image.BufferedImage;
  */
 public class Assets {
     public static BufferedImage background; // to store background image
+    public static BufferedImage menu[];
 
     /**
      * initializing the images of the game
@@ -20,8 +21,18 @@ public class Assets {
     public static void init() {
         SpriteSheet chefRight = new SpriteSheet(ImageLoader.loadImage("/images/chefRight.png"));
         SpriteSheet chefLeft = new SpriteSheet(ImageLoader.loadImage("/images/chefLeft.png"));
-        SpriteSheet menu = new SpriteSheet(ImageLoader.loadImage("/images/menu.png"));
+        SpriteSheet menuSheet = new SpriteSheet(ImageLoader.loadImage("/images/menu.png"));
         background = ImageLoader.loadImage("/images/Background.jpg");
+        menu = new BufferedImage[116];
+        for(int i=0; i<8; i++) {
+            for(int j=0; j<14; j++) {
+                menu[i*8+j] = menuSheet.crop(34*j, 34*i, 34, 34);
+            }
+        }
+        for(int i=0; i<4; i++) {
+            menu[112+i] = menuSheet.crop(34*14, 34*(4+i), 34, 34);
+            System.out.println(34*(4+i));
+        }
     }
     
 }
